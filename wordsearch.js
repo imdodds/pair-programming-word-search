07684b58e3ab2fd5fd3c882c9386bed6a5aa6de0
Ian Dodds <ianmitchelldodds@gmail.com>
2022-10-08 17:21:22 +0000

    Add wordsearch function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+
+    const transpose = (matrix) => {
+        const result = [];
+        for (let column = 0; column < matrix[0].length; column++) {
+            result.push([]);
+            for (let row = 0; row < matrix.length; row++) {
+                result[column].push(matrix[row][column]);
+            }
+        }
+        return result;
     }
-}
+
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+    
+    if (!letters || !word) {
+        return false;
+    }
+
+    for (const l of horizontalJoin) {
+        if (l.includes(word)) return true;
+    }
+    for (const l of verticalJoin) {
+        if (l.includes(word)) return true;
+    }
+    return false;
+
+};
 
 module.exports = wordSearch
